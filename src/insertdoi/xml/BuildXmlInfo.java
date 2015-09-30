915216f56f4d18cb663da255d49e525d97f42001
jcarlos <carlosmaster10@gmail.com>
2015-09-30 12:10:57 -0300

    create BuildXmlInfo class

---
+++ src/insertdoi/xml/BuildXmlInfo.java
@@ -0,0 +1,18 @@
+package insertdoi.xml;
+
+import insertdoi.event.EventData;
+
+public class BuildXmlInfo {
+
+    private EventData eventData = null;
+    
+    public BuildXmlInfo(EventData eventData) {
+        this.eventData = eventData;
+    }
+    
+    public void run() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
